b6f6b7baf36b71485ecc12cb0840c2664e13bf7a
zin9x <thangbk.9x@gmail.com>
2014-08-01 22:56:01 +0700

    Create object Information

---
+++ app/src/main/java/project/banmua/com/item/Information.java
@@ -0,0 +1,14 @@
+package project.banmua.com.item;
+
+/**
+ * Created by Zin9x on 8/1/2014.
+ */
+public class Information {
+    private int code;
+    private String sessionId;
+    private String nickName;
+    private String birthDay;
+    private String kind;
+    private int userType;
+    private String userName;
+}
